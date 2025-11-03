c5a1f9219ee7c3281834388076fbe93e655892a3
YosyGIT <jose.rubinc.1@educa.jcyl.es>
2025-11-03 12:59:32 +0100

    Ejercicio 4 de la practica 16

--- src/practica16/Ej4.java
+++ src/practica16/Ej4.java
@@ -3,24 +3,26 @@ package practica16;
 public class Ej4 {
     public static void main(String[] args) {
         int[] array = new int[10000];
-        int[] numeroDado = new int[6];
+        int[] repeticion = new int[6];
+        int auxRepeticion;
 
         for (int i = 0; i < array.length; i++) {
             array[i] = (int)(Math.random() * (6 - 1 + 1) + 1);
         }
 
         for (int j = 0; j < array.length; j++) {
-            for (int k = 0; k < numeroDado.length; k++) {
+            for (int k = 0; k < repeticion.length; k++) {
                 if(array[j] == k + 1){
-                    numeroDado[k]++;
+                    repeticion[k]++;
                 }
             }
         }
 
-        for (int i = 0; i < numeroDado.length; i++) {
-            System.out.print("El numero " + (i + 1) + " aparece " + numeroDado[i] + " veces: ");
-            while(numeroDado[i] != 0){
-                numeroDado[i] /= 100;
+        for (int i = 0; i < repeticion.length; i++) {
+            System.out.print("El numero " + (i + 1) + " aparece " + repeticion[i] + " veces: ");
+            auxRepeticion = repeticion[i];
+            auxRepeticion /= 100;
+            for (int j = 0; j < auxRepeticion; j++) {
                 System.out.print("*");
             }
             System.out.println();
