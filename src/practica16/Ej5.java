1d92a200a7e068e750cdaac06e6b0a4504e672e8
YosyGIT <jose.rubinc.1@educa.jcyl.es>
2025-11-03 14:05:56 +0100

    Ejercicio 5 de la practica 16

---
+++ src/practica16/Ej5.java
@@ -0,0 +1,20 @@
+package practica16;
+import java.util.*;
+public class Ej5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        int[] numeros = new int[10];
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Introduce un numero: ");
+            numeros[i] = sc.nextInt();
+            for (int j = 0; j < numeros.length; j++) {
+                if (numeros[j] == numeros[i]) {
+                    System.out.println("El numero ya se encuentra en el array");
+                }
+            }
+        }
+    }
+}
